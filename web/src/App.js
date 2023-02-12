6fc086b73373bf685a5cae16a75df1c753680ca1
Andrew Xu <xusandrew@gmail.com>
2023-02-12 18:37:39 -0500

    Add sample board

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,20 @@
 import './App.css'
+import sampleGameData from './samplegame.json'
+import React, { useState, useEffect } from 'react'
 
 function App() {
+  const [boardState, setBoardState] = useState(
+    sampleGameData.games[0].boardData // temp definition
+  )
+
+  function printBoard() {
+    console.log(boardState)
+  }
+
   return (
     <div className='App'>
       <h1>hello</h1>
+      <button onClick={printBoard}>Hi</button>
     </div>
   )
 }
