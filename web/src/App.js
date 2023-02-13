03a89604c67ec0b997e1318b926355fe6f0cac0e
Andrew Xu <xusandrew@gmail.com>
2023-02-12 20:43:21 -0500

    Display board

--- web/src/App.js
+++ web/src/App.js
@@ -1,22 +1,27 @@
 import './App.css'
-import './components/GameDataDisplay.js'
+import './components/GameDataDisplay'
+import './components/Board'
 import sampleGameData from './samplegame.json'
 import React, { useState, useEffect } from 'react'
 import GameDataDisplay from './components/GameDataDisplay.js'
+import Board from './components/Board'
 
 function App() {
   const [boardState, setBoardState] = useState(
     sampleGameData.games[0].boardData // temp definition
   )
   const [boardDifficulty, setBoardDifficulty] = useState(
-    sampleGameData.games[0].difficulty
+    sampleGameData.games[0].difficulty // temp definition
   )
   const [boardIndex, setBoardIndex] = useState(0)
+
+  const [boardSize, setBoardSize] = useState(sampleGameData.games[0].size)
 
   function printBoard() {
     console.log(boardState)
     console.log(boardDifficulty)
     console.log(boardIndex)
+    console.log(boardSize)
   }
 
   return (
@@ -26,6 +31,7 @@ function App() {
         board_difficulty={boardDifficulty}
         board_index={boardIndex}
       />
+      <Board board_state={boardState} board_size={boardSize} />
     </div>
   )
 }
--- web/src/components/board.js
+++ web/src/components/board.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Board = props => {
+  const [boardStyle, setBoardStyle] = useState({})
+
+  useEffect(() => {
+    setBoardStyle({
+      gridTemplateColumns: `repeat(${props.board_size} , 1fr)`,
+      gridTemplateRows: `repeat(${props.board_size} , 1fr)`,
+    })
+  }, [props.board_size])
+
   return (
-    <div className='App'>
-      <h1>hello</h1>
+    <div className='board' style={boardStyle}>
+      {props.board_state.flat().map((square, i) => {
+        return (
+          <div key={i} className='square'>
+            {square}
+          </div>
+        )
+      })}
     </div>
   )
 }
